cb469771a285699e83dcac5452e98ebaacae3f34
Denys Plietnikov <givisimo@gmail.com>
2020-03-16 21:00:00 +0200

    small fix

--- src/Reader/Reader.jsx
+++ src/Reader/Reader.jsx
@@ -59,55 +59,5 @@ Reader.propTypes = {
   location: T.instanceOf(Object).isRequired,
   history: T.instanceOf(Object).isRequired,
 };
-// class Reader extends Component {
-//   state = {};
-//
-
-//
-//   componentDidMount() {
-//     const { history, items } = this.props;
-//     if (this.handlePubInx() <= items.length) {
-//       return;
-//     }
-//     history.replace('/reader?item=1');
-//   }
-//
-//   showPublication = () => {
-//     const pubInxObj = queryString.parse(this.props.location.search);
-//     const pubInx = pubInxObj.item;
-//     const pub = this.props.items[pubInx - 1];
-//     return pub;
-//   };
-//
-//   onButtonClick = item => {
-//     this.props.history.push(`/reader?item=${this.handlePubInx() + item}`);
-//   };
-//
-//   handlePubInx = () => {
-//     const pubInxObj = queryString.parse(this.props.location.search);
-//     const pubInx = pubInxObj.item;
-//     return Number(pubInx);
-//   };
-//
-//   render() {
-//     const { items } = this.props;
-//     const { length } = items;
-//     const publication = this.showPublication();
-//     const prevBtnDisabled = this.handlePubInx() === 1;
-//     const nextBtnDisabled = this.handlePubInx() === length;
-//
-//     return (
-//       <>
-//         <Controls
-//           onButtonClick={this.onButtonClick}
-//           prevBtnDisabled={prevBtnDisabled}
-//           nextBtnDisabled={nextBtnDisabled}
-//         />
-//         <Progress currentPage={this.handlePubInx() - 1} totalPages={length} />
-//         {publication && <Publication publication={publication} />}
-//       </>
-//     );
-//   }
-// }
 
 export default Reader;
